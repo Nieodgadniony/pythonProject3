a63098795beaf87c59e7fb1939fef496e247db2d
Nieodgadniony <stanislawlewylewicki@gmail.com>
2021-10-28 17:34:36 +0200

    Initial commit MAIN

---
+++ main.py
@@ -0,0 +1,20 @@
+def P1(liczba):
+    return liczba * 2
+def P2(liczba):
+    wynik = liczba *2
+    return f"Wynik diałania to: {wynik}"
+
+
+def f1(liczba):
+    return liczba *3 -5
+print(f1(10))
+
+def f2(liczba):
+    return liczba /10
+
+print(f2(10))
+
+def f3():
+    return 0
+
+print(f3())
